c9466c5df6632e0a89e55d28f9e4eda884bb48ef
Doğuş Kar <35219098+doguskar@users.noreply.github.com>
2020-11-03 22:40:40 +0300

    Sign in and Sign up models

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FarmTracker_web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmTracker_web.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string SignIn(SignInRequest request)
+        {
+            return "";
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
