d58379edd781b07a0917ea784b509e70e577d0e3
Doğuş Kar <35219098+doguskar@users.noreply.github.com>
2020-11-04 17:33:58 +0300

    Sign up procceses
    - Is used username and email control
    - Form validations
    - Creating SignUpRequest

---
+++ Models/Members/GeneratedUcodes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FarmTracker_web.Models.DB
+{
+    public partial class GeneratedUcodes
+    {
+        public Guid Guc { get; set; }
+        public Guid? ForUuid { get; set; }
+        public string ForIp { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public bool? IsValid { get; set; }
+    }
+}
--- Models/Members/SignUpResponse.cs
+++ Models/Members/SignUpResponse.cs
@@ -5,5 +5,6 @@ namespace FarmTracker_web.Models.Members
     public class SignUpResponse
     {
         public bool Result { get; set; }
+        public bool TooManyAttempts { get; set; }
     }
 }
