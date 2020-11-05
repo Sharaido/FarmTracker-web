69925ee8e1ddee58db03811e6bba2ec43749f1d7
Doğuş Kar <35219098+doguskar@users.noreply.github.com>
2020-11-05 11:59:58 +0300

    Getting farms
    - Created Farms model
    - Created Farms controller
    - Getting farms with ajax (farmTracker.js)
    - Showing farms (dkNav)
    - Showing farm (Farms/Index.cshtml)
    - dkNav some fixes
    - dkNav links updated

---
+++ Models/Farms/Farms.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FarmTracker_web.Models.DB
+{
+    public partial class Farms
+    {
+        public Farms()
+        {}
+
+        public Guid Fuid { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid CreatedByUuid { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public bool DeletedFlag { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public Guid? DeletedByUuid { get; set; }
+    }
+}
