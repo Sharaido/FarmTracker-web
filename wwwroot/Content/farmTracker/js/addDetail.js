ce84c63ebeae385536a14658ad17e1dbec03dc0d
Doğuş Kar <35219098+doguskar@users.noreply.github.com>
2021-01-15 11:25:26 +0300

    post add CP problem fixed

---
+++ wwwroot/Content/farmTracker/js/addDetail.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(() => {
+    if ($("#categoryProperties").length > 0 && $("#hiddenCUID").length > 0 && $("#hiddenCUID").val()) {
+        getCategoryProperties($("#hiddenCUID").val())
+    }
+})
+function getAddCPValues(AUID) {
+    $.ajax({
+        type: "GET",
+        url: "/Adds/AddCOPValues/" + AUID,
+        success: function (values) {
+            if (values) {
+                printAddCPValues(values)
+            }
+        }
+    })
+}
+function printAddCPValues(values) {
+    for (value of values) {
+        if (value.value == "on") {
+            $('#CP_' + value.puid).html("Yes")
+        } else {
+            $('#CP_' + value.puid).html(value.value)
+        }
+
+    }
+}
+function getCategoryProperties(CUID) {
+    $.ajax({
+        type: "GET",
+        url: "/Farms/CategoryProperties/" + CUID,
+        success: function (cProperties) {
+            if (cProperties) {
+                printCategoryPropertiesInputs(cProperties)
+                if ($("#hiddenAUID").length > 0) {
+                    getAddCPValues($("#hiddenAUID").val())
+                }
+            } else {
+                $('#categoryProperties').html("")
+            }
+        }
+    })
+}
+function printCategoryPropertiesInputs(cProperties) {
+    var body = ''
+    for (var property of cProperties) {
+        body += `<label><b>${property.name}: </b><span id="CP_${property.puid}"></span></label>`
+    }
+    $('#categoryProperties').html(body)
+}
